85819a188422768340265399c4159fbd652345a7
mynfred <javawindow@163.com>
2021-01-30 21:18:01 +0800

    append some code .....

--- 5-5/5_5.py
+++ 5-5/5_5.py
@@ -4,3 +4,11 @@ import matplotlib.pyplot as plt
 # 真正的函数
 def g(x):
     return 0.1 * (x ** 3 + x ** 2 + x)
+
+
+# 随意准备一些向真正的函数加入了一点噪声的训练数据
+train_x = np.linspace(-2, 2, 8)
+
+
+# 随意准备一些向真正的函数加入了一点噪声的训练数据
+train_x = np.linspace(-2, 2, 8)
